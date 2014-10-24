3a5c23040121eb2593631db4bf8c256f9c1cd2c2
Simon Mieth <sm@ethinking.de>
2014-10-24 18:30:58 +0200

    initialize date subproject

---
+++ amajza-date/src/main/java/de/ethinking/amajza/tags/LoopDateTag.java
@@ -0,0 +1,48 @@
+package de.ethinking.amajza.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class LoopDateTag extends TagSupport{
+
+	/**
+	 * 
+	 */
+    private static final long serialVersionUID = 1L;
+    
+    
+    protected String var;
+
+
+	@Override
+    public int doStartTag() throws JspException {
+	 
+	    return super.doStartTag();
+    }
+
+
+	@Override
+    public int doEndTag() throws JspException {
+	 
+	    return super.doEndTag();
+    }
+
+
+	@Override
+    public int doAfterBody() throws JspException {
+	   
+	    return super.doAfterBody();
+    }
+
+
+	@Override
+    public void release() {
+	  
+	    super.release();
+    }
+    
+    
+   
+    
+    
+}
